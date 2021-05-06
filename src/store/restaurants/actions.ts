cd4bd5b9b9d1564b8445f3fe96695fab4ded274c
Hans Delano <hanscau@hotmail.com>
2021-05-06 15:30:16 +0800

    Add: Redux action implementation

--- src/store/restaurants/actions.ts
+++ src/store/restaurants/actions.ts
@@ -1,1 +1,16 @@
-export const loadRestaurants = () => () => {};
+export const STORE_RESTAURANTS = 'STORE_RESTAURANTS';
+
+export const loadRestaurants = () => (
+  dispatch: any,
+  getState: any,
+  api: any,
+) => {
+  api.loadRestaurants().then((records: any) => {
+    dispatch(storeRestaurants(records));
+  });
+};
+
+const storeRestaurants = (records: any) => ({
+  type: STORE_RESTAURANTS,
+  records,
+});
